41ff2dd29c12860564f05c2e60805db1170a2e31
Rafay Hassan <rafayhassan02@gmail.com>
2024-11-10 18:39:46 -0500

    Update CalculatorTest.java

--- Homework 1/src/CalculatorTest.java
+++ Homework 1/src/CalculatorTest.java
@@ -1,18 +1,16 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class CalculatorTest {
 
     @Test
     void add() {
         assertEquals(4, Calculator.add(2, 2));
-        assertEquals(3, Calculator.add(1, 2));
     }
 
     @Test
     void multiply() {
-        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+        assertAll(() -> assertEquals(3, Calculator.multiply(2, 2)),
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
                 () -> assertEquals(4, Calculator.multiply(-2, -2)),
                 () -> assertEquals(0, Calculator.multiply(1, 0)));
