c867483658bbc89d7a4b5a05afda9a197158ce06
Rafay Hassan <rafayhassan02@gmail.com>
2024-11-10 18:55:50 -0500

    testing confirmed

--- Homework2/src/Main.java
+++ Homework2/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static int testing(int a, int b){
+        return a*b;
     }
 }
