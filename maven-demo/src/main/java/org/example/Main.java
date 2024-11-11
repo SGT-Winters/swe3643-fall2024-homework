134d705fd64a52c6b643e8b1154cdc4a1b458774
Rafay Hassan <rafayhassan02@gmail.com>
2024-11-10 22:03:33 -0500

    playwright

---
+++ maven-demo/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Playwright pw = Playwright.create();
+
+        BrowserType browserType = pw.chromium();
+
+        Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Page page = browser.newPage();
+
+        page.navigate("https://www.pokemoncenter.com/");
+
+        String title = page.title();
+
+        System.out.println("The title is: " + title);
+
+        //page.close();
+        //browser.close();
+        //pw.close();
+
+    }
+}
