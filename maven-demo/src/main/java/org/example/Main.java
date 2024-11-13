a6dc5d6ae23c9013e18df41b4d2c96051eb847b0
Rafay Hassan <rafayhassan02@gmail.com>
2024-11-12 19:29:58 -0500

    done

--- maven-demo/src/main/java/org/example/Main.java
+++ maven-demo/src/main/java/org/example/Main.java
@@ -1,15 +1,30 @@
 package org.example;
 
-import com.microsoft.playwright.*;
-import java.nio.file.Paths;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 public class Main {
     public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.webkit().launch();
-            Page page = browser.newPage();
-            page.navigate("https://playwright.dev/");
-            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("example.png")));
-        }
+
+        Playwright pw = Playwright.create();
+
+        BrowserType browserType = pw.chromium();
+
+        Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Page page = browser.newPage();
+
+        page.navigate("https://allpoketcg.com/");
+
+        String title = page.title();
+
+        System.out.println("The title is: " + title);
+
+        //page.close();
+        //browser.close();
+        //pw.close();
+
     }
 }
