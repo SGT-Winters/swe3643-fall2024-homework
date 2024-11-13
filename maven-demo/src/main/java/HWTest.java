101a8535e4258c192cfa57c4e562b037fb9966d2
Rafay Hassan <rafayhassan02@gmail.com>
2024-11-12 20:00:08 -0500

    Homework 3 Done

--- maven-demo/src/main/java/HWTest.java
+++ maven-demo/src/main/java/HWTest.java
@@ -1,3 +1,4 @@
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.junit.UsePlaywright;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.*;
@@ -11,8 +12,15 @@ public class HWTest {
     void test(Page page) {
         page.navigate("https://demo.playwright.dev/todomvc/#/");
         page.getByPlaceholder("What needs to be done?").click();
-        page.getByPlaceholder("What needs to be done?").fill("Complete Homework 3");
+        page.getByPlaceholder("What needs to be done?").fill("Working");
         page.getByPlaceholder("What needs to be done?").press("Enter");
+        page.getByPlaceholder("What needs to be done?").fill("Eating");
+        page.getByPlaceholder("What needs to be done?").press("Enter");
+        page.getByPlaceholder("What needs to be done?").fill("Completeing Homework 3");
+        page.getByPlaceholder("What needs to be done?").press("Enter");
+        page.locator("li").filter(new Locator.FilterOptions().setHasText("Eating")).getByLabel("Toggle Todo").check();
+        page.locator("li").filter(new Locator.FilterOptions().setHasText("Completeing Homework")).getByLabel("Toggle Todo").check();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Clear completed")).click();
         page.getByLabel("Toggle Todo").check();
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Clear completed")).click();
     }
